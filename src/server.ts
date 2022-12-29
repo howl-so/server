31219ef780cab8bb652fb367bf7ce605c02bc876
mramotar <mramotar@dropbox.com>
2022-12-28 19:06:37 -0500

    Remove neo4j

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import { connect } from "mongoose";
 import "reflect-metadata";
 import app from "./app";
-import { DEV, MONGODB_URI, NODE_ENV, PORT, SEED } from "./util/secrets";
 import seed from "./seed/seed";
-import graph from "./graph";
+import { DEV, MONGODB_URI, NODE_ENV, PORT, SEED } from "./util/secrets";
 
 const main = async () => {
   await connect(MONGODB_URI!);
@@ -12,15 +11,14 @@ const main = async () => {
     await seed();
   }
 
-  const graphServerInfo = await graph.getServerInfo();
+  try {
+  } catch (error) {}
+
   const apiServerAddress = NODE_ENV == DEV ? `http://localhost:${PORT}` : "https://api.howl.so/v1";
 
   app.listen({ port: PORT }, () => {
-    console.log(`🚀 Graph running at => ${graphServerInfo.address} `);
     console.log(`🚀 Server running at => ${apiServerAddress}`);
   });
 };
 
-main()
-  .catch((error) => console.log(`💥 Error: ${error}`))
-  .finally(() => graph.close());
+main().catch((error) => console.log(`💥 Error: ${error}`));
