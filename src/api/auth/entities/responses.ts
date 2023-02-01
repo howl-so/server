d3b6434a28d70082c9f53c198b57bbbf8a534f15
mramotar <mramotar@dropbox.com>
2023-01-31 21:00:05 -0500

    Add AuthenticatedHowlUser
    Signed-off-by: mramotar <mramotar@dropbox.com>

--- src/api/auth/entities/responses.ts
+++ src/api/auth/entities/responses.ts
@@ -5,11 +5,11 @@ export interface SuccessfulAuth {
   token: string;
 }
 
-export class ContinueWithGoogleSuccess implements SuccessfulAuth {
-  readonly user: User;
+export class ContinueWithGoogleSuccess {
+  readonly user: AuthenticatedHowlUser;
   readonly token: string;
 
-  constructor(user: User, token: string) {
+  constructor(user: AuthenticatedHowlUser, token: string) {
     this.user = user;
     this.token = token;
   }
@@ -22,5 +22,37 @@ export class TokenValidationSuccess implements SuccessfulAuth {
   constructor(user: User, token: string) {
     this.user = user;
     this.token = token;
+  }
+}
+
+export class AuthenticatedHowlUserHowler {
+  _id: string;
+  name: string;
+  avatarUrl?: string;
+  ownerIds: string[];
+
+  constructor(_id: string, name: string, ownerIds: string[], avatarUrl?: string) {
+    this._id = _id;
+    this.name = name;
+    this.ownerIds = ownerIds;
+    this.avatarUrl = avatarUrl;
+  }
+}
+
+export class AuthenticatedHowlUser {
+  _id: string;
+  name: string;
+  email: string;
+  username: string;
+  avatarUrl?: string;
+  howlers: AuthenticatedHowlUserHowler[];
+
+  constructor(_id: string, name: string, email: string, username: string, howlers: AuthenticatedHowlUserHowler[], avatarUrl?: string) {
+    this._id = _id;
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.avatarUrl = avatarUrl;
+    this.howlers = howlers;
   }
 }
--- src/api/auth/services/continueWithGoogle.ts
+++ src/api/auth/services/continueWithGoogle.ts
@@ -1,14 +1,16 @@
 import jwt from "jsonwebtoken";
+import RealHowlerServices from "../../../api/howlers/services/RealHowlerServices";
 import { UserModel } from "../../../models";
 import { KEYS } from "../../../util/secrets";
 import RealUserServices from "../../users/services/RealUserServices";
 import { ContinueWithGoogleInput } from "../entities/LoginInput";
-import { ContinueWithGoogleSuccess } from "../entities/responses";
+import { AuthenticatedHowlUser, AuthenticatedHowlUserHowler, ContinueWithGoogleSuccess } from "../entities/responses";
 
 export default async function continueWithGoogle(input: ContinueWithGoogleInput): Promise<ContinueWithGoogleSuccess> {
   const { email, username, name, googleId, avatarUrl } = input;
 
   const userService = new RealUserServices();
+  const howlerServices = new RealHowlerServices();
 
   let user = await UserModel.findOne({ username });
 
@@ -20,5 +22,21 @@ export default async function continueWithGoogle(input: ContinueWithGoogleInput)
   await user.save();
 
   const token = jwt.sign({ userId: user._id }, KEYS!);
-  return new ContinueWithGoogleSuccess(user, token);
+
+  const howlers: AuthenticatedHowlUserHowler[] = [];
+
+  const howlerIds = user.howlerIds ?? [];
+
+  for (const howlerId of howlerIds) {
+    const howler = await howlerServices.getHowler(howlerId);
+
+    if (howler) {
+      const authenticatedHowlUserHowler = new AuthenticatedHowlUserHowler(howler._id, howler.name, howler.ownerIds, howler.avatarUrl);
+      howlers.push(authenticatedHowlUserHowler);
+    }
+  }
+
+  const authenticatedHowlUser = new AuthenticatedHowlUser(user._id, user.name, user.email, user.username, howlers, user.avatarUrl);
+
+  return new ContinueWithGoogleSuccess(authenticatedHowlUser, token);
 }
