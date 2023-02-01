9e7100b26762fe2451f28dbb50a00f8d0c89b0f7
mramotar <mramotar@dropbox.com>
2023-02-01 05:25:50 -0500

    Fix route
    Signed-off-by: mramotar <mramotar@dropbox.com>

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,6 @@ import RealAuthService from "./api/auth/services/AuthService";
 
 const router = express.Router();
 
-router.post("/auth/continue/google", new RealAuthService().continueWithGoogle);
 router.post("/auth/token/validate", new RealAuthService().validateToken);
 
 export default router;
